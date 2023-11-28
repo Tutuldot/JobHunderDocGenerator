338d84d722009a682dc53541cd8edeb7bcb83c6f
Anthony Estrada <azestrada@outlook.com>
2023-11-28 06:24:00 +0000

    test

--- demo.py
+++ demo.py
@@ -5,12 +5,12 @@ import json
 
 url = 'https://api.deepinfra.com/v1/inference/meta-llama/Llama-2-70b-chat-hf'
 headers = {
-    'Authorization': f'Bearer ABC',
+    'Authorization': f'Bearer HTj80cPBu4Qaiw80oIUZAs6J9Nzg73XK',
     'Content-Type': 'application/json'
 }
 data = {
-    'input': 'Current Employer: StMicroelectronics as Senior Data Analyst for 1 year, Previous Employer: Asurion as Senior Business Intelligence Developer for 6 years, skills: Skills: Data Modelling, Data Visualization, Database Design, Data management, ETL Development,   SQL Scripting, Data Management and Data Analysis. please generate a cover letter for senior data analyst at ABC Corporation.'
-}
+    'input': 'Current Employer: Sun Crest Fruits for 1 year, Previous Employer: Del Monte Coporation as Fruit Sorter for 6 years, Skills: Efficient interpersonal skills, Hardworking,Flexibility, Reliability, Team player . please generate a cover letter for senior data analyst at ABC Corporation.'
+}, 
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
 print(response.json())
